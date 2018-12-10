9d3d2e5c029d03bdd255ab8e843247c37b1a8333
JB <jakesh.bohaju@gmail.com>
2018-12-10 12:32:27 +0545

    Update move_folder_to_other_folder.py

--- move_folder_to_other_folder.py
+++ move_folder_to_other_folder.py
@@ -1,12 +1,12 @@
 import os
-BASE_DIR = r'C:\Users\jxeni\Desktop\scripting_organizing_file'
+BASE_DIR = r'C:\Users\jxeni\Desktop\scripting_organizing_file'  # here must be your path
 MASTER_FOLDER = r'root'
 
 if not os.path.exists(os.path.join(BASE_DIR,MASTER_FOLDER)):
     os.mkdir(os.path.join(BASE_DIR,MASTER_FOLDER))
 
 for item in os.listdir(BASE_DIR):
-    if os.path.isdir(os.path.join(BASE_DIR, item)):
+    if os.path.isdir(os.path.join(BASE_DIR, item)) and not item == MASTER_FOLDER:
         os.rename(
             os.path.join(BASE_DIR, item),
             os.path.join(BASE_DIR, MASTER_FOLDER, item)
