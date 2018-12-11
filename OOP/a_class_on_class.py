b16550573b5417f5e9e881dc6310072311e7435d
JB <jakesh.bohaju@gmail.com>
2018-12-11 18:51:49 +0545

    Add files via upload

--- OOP/a_class_on_class.py
+++ OOP/a_class_on_class.py
@@ -6,6 +6,8 @@
 # self, with object call method is self, called through dot operator. example quard() called by a then self send its value as an argument
 # dot operator use to access member of class i.e attributes and method
 # constructor, pass value on object creation, here def __init__(self, x, y) use for constructor
+# dot operator access member i.e object
+# by default on print it convert to string of and type
 
 class Point:
     # x = None
@@ -27,6 +29,19 @@ class Point:
         if self.x > 0 and self.y < 0:
             return 4
 
+    # cast object to string, Point({1st},{2nd}).format(self.x,self.y) => x goes to 1st and y goes to 2nd
+    # it must return string
+    def __str__(self):
+        return 'Point({},{})'.format(self.x, self.y)
+
+    # cast to integer
+    def __int__(self):
+        return 1
+
+    # cast to float
+    def __float__(self):
+        return 2.2
+
 
 a = Point(3, 4)
 
@@ -34,9 +49,14 @@ a = Point(3, 4)
 # a.y = 4
 
 print(a.quard())
+print(
+    a)  # give output like:  <__main__.Point object at 0x000001F639FCA7B8>, its means this file that is main and Point class and memory location
 
 b = Point(7, -8)
 # b.x = 7
 # b.y = -8
 
 print(b.quard())
+print(b)
+
+print(int(a) + 2)  # it calls,  def __int__(self): function of the class
